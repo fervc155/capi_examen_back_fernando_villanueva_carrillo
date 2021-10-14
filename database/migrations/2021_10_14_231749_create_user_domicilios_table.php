0603c2ae92f147950c4e35a4cca880202fa405c9
Fernando Villanueva <fervillanueva.1998@hotmail.com>
2021-10-14 18:38:19 -0500

    crea una migracion

---
+++ database/migrations/2021_10_14_231749_create_user_domicilios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilios', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_domicilio',254);
+            $table->foreignId('user_id')->constrained();
+            $table->integer('numero_exterior');
+            $table->string('colonia',100);
+            $table->string('cp',5);
+            $table->string('ciudad',100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilios');
+    }
+}
